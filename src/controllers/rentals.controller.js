898f816f8d957763b22539c7369dd9c528c89366
thiago-arend <thiago.arend@gmail.com>
2023-07-29 02:30:58 -0300

    feat: rota post /rentals

---
+++ src/controllers/rentals.controller.js
@@ -0,0 +1,28 @@
+import { db } from "../database/database.connection.js";
+import dayjs from "dayjs";
+
+export async function createRental(req, res) {
+    const { customerId, gameId, daysRented } = req.body;
+
+    try {
+        const game = await db.query("SELECT * FROM games WHERE id=$1;", [gameId]);
+        if (game.rowCount === 0) return res.status(400).send("Id do jogo é inválido!");
+
+        const customer = await db.query("SELECT * FROM customers WHERE id=$1;", [customerId]);
+        if (customer.rowCount === 0) return res.status(400).send("Id do cliente é inválido!");
+
+        const totalAlugueisAberto = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1 AND "returnDate" IS NULL;`, [gameId]);
+        if (totalAlugueisAberto.rowCount >= game.rows[0].stockTotal) return res.status(400).send("Não há jogos disponíveis para aluguel.");
+
+        const originalPrice = daysRented * game.rows[0].pricePerDay;
+        const rentDate = dayjs().format("YYYY-MM-DD");
+        const values = [customerId, gameId, rentDate, daysRented, null, originalPrice, null];
+        await db.query(
+            `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+            VALUES ($1, $2, $3, $4, $5, $6, $7);`, values);
+
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
---
+++ src/routes/rentals.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { validateSchema } from "../middlewares/validadeSchema.js";
+import { rentalsSchema } from "../schemas/rentals.schema.js";
+import { createRental } from "../controllers/rentals.controller.js";
+
+const rentalsRouter = Router();
+
+rentalsRouter.post("/rentals", validateSchema(rentalsSchema), createRental);
+
+export default rentalsRouter;
